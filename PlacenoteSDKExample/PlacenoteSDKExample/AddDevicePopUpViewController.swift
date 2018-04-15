f4be960d01319615b5ca569c6f16defc6c66a0a7
imittal <ishaani.94@gmail.com>
2018-04-15 17:53:23 -0400

    pop up for adding device

--- PlacenoteSDKExample/PlacenoteSDKExample/AddDevicePopUpViewController.swift
+++ PlacenoteSDKExample/PlacenoteSDKExample/AddDevicePopUpViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class AddDevicePopUpViewController: UIViewController {
 
+    @IBOutlet weak var deviceNameInput: UITextField!
+    @IBOutlet weak var functionInput: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.view.backgroundColor = UIColor.black.withAlphaComponent(0.8)
+        self.showAnimate()
         // Do any additional setup after loading the view.
     }
 
@@ -21,7 +26,34 @@ class AddDevicePopUpViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func saveDevice(_ sender: Any) {
+       self.removeAnimate()
+    }
+    
+    
+    func showAnimate()
+    {
+        self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+        self.view.alpha = 0.0;
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.alpha = 1.0
+            self.view.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        });
+    }
+    
+    func removeAnimate()
+    {
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+            self.view.alpha = 0.0;
+        }, completion:{(finished : Bool)  in
+            if (finished)
+            {
+                self.view.removeFromSuperview()
+            }
+        });
+    }
+    
     /*
     // MARK: - Navigation
 
