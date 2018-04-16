8b1473f4af2bf20237add718d1c8b98de4d543d3
Hope Idaewor <ellohope@lawn-128-61-120-175.lawn.gatech.edu>
2018-04-15 22:33:38 -0400

    changes

--- PlacenoteSDKExample/PlacenoteSDKExample/HomeViewController.swift
+++ PlacenoteSDKExample/PlacenoteSDKExample/HomeViewController.swift
@@ -12,56 +12,10 @@ import SceneKit
 import ARKit
 import PlacenoteSDK
 
-class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
-    
-    let elements = ["dog", "cat"]
+class HomeViewController: UIViewController{
     
     @IBOutlet weak var tableView: UITableView!
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return globalMap.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        /*
-        let map = globalMap[indexPath.row]
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: map.0) as! CustomTableViewCell
-        
-        //cell.textLabel!.text = map.0 //
-        
-        //var subtitle = "Distance Unknown"
-        
-        //cell.detailTextLabel!.text = subtitle
-        
-        
-        print("globalmap is: ", globalMap)
-        
-        return cell
-        */
-        
-        if (globalMap.isEmpty){
-            print("No Maps Found")
-            let cell = tableView.dequeueReusableCell(withIdentifier: "customCell") as! CustomTableViewCell
-            cell.customLabel.text = "No Maps Found"
-            return cell
-            
-        }
-        else{
-            let map = globalMap[indexPath.row]
-            print("globalMap is: ", map)
-        
-            let cell = tableView.dequeueReusableCell(withIdentifier: "customCell") as! CustomTableViewCell
-            
-            cell.customLabel.text = map.0
-     
-            return cell
-        }
-    }
-
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 100
-    }
 
     @IBAction func addButtonPressed(_ sender: UIButton) {
         
@@ -73,8 +27,7 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.delegate = self
-        tableView.dataSource = self
+     
         // Do any additional setup after loading the view.
     }
 
